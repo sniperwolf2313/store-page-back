b6c9acbfa27ecb57af2961db5ea0e470cb81f85e
= <=>
2024-08-01 02:35:22 -0500

    Ajustes entity Product, Resuelve #2

--- src/domain/entities/product.entity.ts
+++ src/domain/entities/product.entity.ts
@@ -1,10 +1,8 @@
 export class Product {
-  constructor(
-    public readonly productId: string,
-    public readonly productName: string,
-    public readonly price: number,
-    public readonly description: string,
-    public readonly stock: number,
-    public readonly imageURL: string,
-  ) {}
+  productId: string;
+  productName: string;
+  price: number;
+  description: string;
+  stock: number;
+  imageURL: string;
 }
