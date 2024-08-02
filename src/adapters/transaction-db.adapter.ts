d2ca1b5800cff5618d2db26490c50402e395a439
= <=>
2024-08-02 00:34:30 -0500

    Creacion Transaction Adapter, Resuelve #54

---
+++ src/adapters/transaction-db.adapter.ts
@@ -0,0 +1,62 @@
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  UpdateCommand,
+  UpdateCommandInput,
+} from '@aws-sdk/lib-dynamodb';
+import { Inject, Injectable } from '@nestjs/common';
+import { Transaction } from 'src/domain/entities/transaction.entity';
+
+@Injectable()
+export class TransactionDbAdapter {
+  constructor(
+    @Inject('DYNAMO_DB')
+    private readonly dynamoDBClient: DynamoDBDocumentClient,
+  ) {}
+
+  async createTransactionDB(transaction: Transaction): Promise<Transaction> {
+    const params = {
+      TableName: 'Transactions',
+      Item: transaction,
+    };
+    const command = new PutCommand(params);
+    await this.dynamoDBClient.send(command);
+    return transaction;
+  }
+
+  async getTransactionDB(transactionId: string): Promise<Transaction> {
+    const params = {
+      TableName: 'Transactions',
+      Key: {
+        transactionId: transactionId,
+      },
+    };
+    const command = new GetCommand(params);
+    const result = await this.dynamoDBClient.send(command);
+    return result.Item as Transaction;
+  }
+
+  async updateTransactionDB(
+    transactionId: string,
+    status: string,
+  ): Promise<Transaction> {
+    const params = {
+      TableName: 'Transactions',
+      Key: {
+        transactionId: transactionId,
+      },
+      UpdateExpression: 'set #status = :status',
+      ExpressionAttributeNames: {
+        '#status': 'status',
+      },
+      ExpressionAttributeValues: {
+        ':status': status,
+      },
+      ReturnValues: 'ALL_NEW',
+    };
+    const command = new UpdateCommand(params as UpdateCommandInput);
+    const result = await this.dynamoDBClient.send(command);
+    return result.Attributes as Transaction;
+  }
+}
