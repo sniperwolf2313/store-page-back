625d6d5f479460801dba1ba52a2709a996ba8b58
= <=>
2024-08-03 23:34:42 -0500

    Se agregan providers adicionales Transaction Entity

--- src/modules/transaction.module.ts
+++ src/modules/transaction.module.ts
@@ -5,10 +5,22 @@ import { DynamoDBModule } from './dynamodb.module';
 import { TransactionDbAdapter } from 'src/adapters/transaction-db.adapter';
 import { PayService } from '../application/services/api-client.service';
 import { HttpModule } from '@nestjs/axios';
+import { CustomerService } from 'src/application/services/customer.service';
+import { DeliveryService } from 'src/application/services/delivery.service';
+import { CustomerDbAdapter } from 'src/adapters/customer-db.adapter';
+import { DeliveryDbAdapter } from 'src/adapters/delivery-db.adapter';
 
 @Module({
   imports: [DynamoDBModule, HttpModule],
-  providers: [TransactionService, TransactionDbAdapter, PayService],
+  providers: [
+    TransactionService,
+    TransactionDbAdapter,
+    PayService,
+    CustomerService,
+    CustomerDbAdapter,
+    DeliveryService,
+    DeliveryDbAdapter,
+  ],
   controllers: [TransactionController],
 })
 export class TransactionModule {}
