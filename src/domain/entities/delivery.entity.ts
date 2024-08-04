56da127a8393017d99be7914b671f2416ee72119
= <=>
2024-08-03 23:35:32 -0500

    Se ajustan valores de Delivery Entity, Resuelve #9

--- src/domain/entities/delivery.entity.ts
+++ src/domain/entities/delivery.entity.ts
@@ -1,11 +1,5 @@
 export class Delivery {
   deliveryId: string;
-  transactionId: string;
-  customerId: string;
-  address_line1: string;
-  address_line2: string;
-  country: string;
-  city: string;
+  shippingData: any;
   status: string;
-  deliveryDate: Date;
 }
